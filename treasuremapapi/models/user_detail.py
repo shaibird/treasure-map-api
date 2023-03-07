9048f3c794abfe4654cdee3cd5e7562c7c8e27f0
Shaina Couch <shainacouch21@gmail.com>
2023-03-07 11:34:28 -0500

    fixtures created, small tweaks to models

--- treasuremapapi/models/user_detail.py
+++ treasuremapapi/models/user_detail.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 class UserDetail(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_details')
-    username = models.CharField(max_length=25)
-    location = models.CharField(max_length=30)
+    bio = models.CharField(max_length=30)
     birthday = models.DateField()
     photo_url = models.URLField(max_length=800)
