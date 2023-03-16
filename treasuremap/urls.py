7e408a0149a6cb3ed4a70d95534190a521dd78d3
Shaina Couch <shainacouch21@gmail.com>
2023-03-16 19:10:29 +0100

    can add layers and get layers

--- treasuremap/urls.py
+++ treasuremap/urls.py
@@ -18,12 +18,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from treasuremapapi.views import ImageView, LocationView, login_user, register_user, LocationNoteView
+from treasuremapapi.views import ImageView, LocationView, login_user, register_user, LocationNoteView, LayerView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'images', ImageView)
 router.register(r'locations', LocationView, 'location')
 router.register(r'locationnotes', LocationNoteView, 'locationnotes')
+router.register(r'layers', LayerView, 'layers')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- treasuremapapi/views/__init__.py
+++ treasuremapapi/views/__init__.py
@@ -1,5 +1,5 @@
 from .image_view import ImageView
 from .location_view import LocationView, LocationSerializer
 from .auth import login_user, register_user
-
+from .layer_view import LayerView
 from .location_notes_view import LocationNoteView
