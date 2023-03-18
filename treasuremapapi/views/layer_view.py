6b36cbb48cb400c5f55ce2c804a1253691914e3f
Shaina Couch <shainacouch21@gmail.com>
2023-03-18 10:53:22 +0100

    can add layers

--- treasuremapapi/views/layer_view.py
+++ treasuremapapi/views/layer_view.py
@@ -7,6 +7,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from treasuremapapi.models import LayerName, LayerPin
+from treasuremapapi.views import LocationSerializer
 
 
 class LayerView(ViewSet):
@@ -28,21 +29,34 @@ class LayerView(ViewSet):
         Returns:
             Response -- JSON serialized list of locations
         """
-
         layer_name = request.query_params.get('layername')
+        user_id = request.query_params.get('user')
+        
         if layer_name is not None:
             try:
                 layer = LayerName.objects.get(name=layer_name)
             except LayerName.DoesNotExist:
                 return Response({'error': 'Layer not found'}, status=status.HTTP_404_NOT_FOUND)
 
-            pins = LayerPin.objects.filter(layer=layer)
+            pins = None
+            if user_id is not None:
+                pins = LayerPin.objects.filter(layer=layer, user=user_id)
+            else:
+                pins = LayerPin.objects.filter(layer=layer)
+            
             serializer = LayerPinSerializer(pins, many=True)
+            return Response(serializer.data)
+        
+        elif user_id is not None:
+            pins = LayerName.objects.filter(user=user_id)
+            serializer = LayerNameSerializer(pins, many=True)
             return Response(serializer.data)
         
         layer_names = LayerName.objects.all()
         serializer = LayerNameSerializer(layer_names, many=True)
         return Response(serializer.data)
+
+
 
     def create(self, request):
 
@@ -76,9 +90,16 @@ class LayerView(ViewSet):
         layer_names.delete()
         
         return Response(None, status=status.HTTP_204_NO_CONTENT)
-
+class LayerPinSerializer(serializers.ModelSerializer):
+    location = LocationSerializer
+     
+    class Meta:
+        model = LayerPin
+        fields = ('id', 'layer', 'location')
+        
 class LayerNameSerializer(serializers.ModelSerializer):
+    pins = LayerPinSerializer(many=True, read_only=True)
 
     class Meta:
         model = LayerName
-        fields = ('id', 'name', 'user')
+        fields = ('id', 'name', 'user', 'pins')
