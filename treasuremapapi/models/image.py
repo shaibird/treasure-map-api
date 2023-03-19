6fd69fc72e38b52b0633d8a1a7c700756b36c1cc
Shaina Couch <shainacouch21@gmail.com>
2023-03-19 11:21:56 +0100

    adding logic for image upload

--- treasuremapapi/models/image.py
+++ treasuremapapi/models/image.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 def upload_to(instance, filename):
-    return 'images/{filename}'.format(filename=filename)
+    return f'user_{instance.user.id}/{filename}'
 
 class Image(models.Model):
     image = models.ImageField(upload_to=upload_to, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_images')
     location = models.ForeignKey("Location", on_delete=models.CASCADE, related_name="location_images")
     private = models.BooleanField(default=True)
+    date = models.DateField(auto_now_add=True)
